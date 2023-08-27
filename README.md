3827a90b6b609a42651066f096d732d5922b0386
DakshSuryavanshi <dakshsuryavanshi2003@gmail.com>
2023-08-27 12:33:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99136c7042f7807e7d80076ed6ddc7bda348560a4a2fbb3e9c350dba3ffba275
+size 14
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:073611131fd37875cb6b9f9e1845c6668f2e00b284390459c0418a436b060f7a
+size 2455
